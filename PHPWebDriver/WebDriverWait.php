73d284a7561dfcba2bcd904046b63a096999a452
Adam Goucher <adam@goucher.ca>
2011-12-09 14:09:26 -0500

    calling the session a session rather than driver

--- PHPWebDriver/WebDriverWait.php
+++ PHPWebDriver/WebDriverWait.php
@@ -3,8 +3,8 @@
 include_once('WebDriverExceptions.php');
 
 class PHPWebDriver_WebDriverWait {
-    public function __construct($driver, $timeout = 30, $poll_frequency = 0.5) {
-      $this->driver = $driver;
+    public function __construct($session, $timeout = 30, $poll_frequency = 0.5) {
+      $this->session = $session;
       $this->timeout = $timeout;
       $this->poll = $poll_frequency;
       return $this;
@@ -13,7 +13,7 @@ class PHPWebDriver_WebDriverWait {
     public function until($func) {
         foreach (range(0, max(1, $this->timeout/$this->poll)) as $iter) {
             try {
-                return call_user_func($func, $this->driver);
+                return call_user_func($func, $this->session);
             } catch (PHPWebDriver_NoSuchElementWebDriverError $e) {
 
             }
