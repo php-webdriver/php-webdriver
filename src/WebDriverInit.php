85bdfdafff54b3886414c62c345c3ce696932884
c9s <cornelius.howl@gmail.com>
2012-01-05 12:28:55 +0800

    Move files into src/

--- src/WebDriverInit.php
+++ src/WebDriverInit.php
@@ -12,7 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 require_once('WebDriverBase.php');
 require_once('WebDriver.php');
 require_once('WebDriverContainer.php');
