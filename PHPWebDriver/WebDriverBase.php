2b58fe9e1163e65ec109d5c9297198768b2ff613
Adam Goucher <adam@goucher.ca>
2012-01-24 23:22:17 -0500

    merging over from https://github.com/facebook/php-webdriver/commit/e780c73982925742e796a1f81766d21daccd05f5

--- PHPWebDriver/WebDriverBase.php
+++ PHPWebDriver/WebDriverBase.php
@@ -133,8 +133,12 @@ abstract class PHPWebDriver_WebDriverBase {
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER,
-                array('application/json;charset=UTF-8'));
+    curl_setopt(
+      $curl,
+      CURLOPT_HTTPHEADER,
+      array(
+        'Content-Type: application/json;charset=UTF-8',
+        'Accept: application/json'));
 
     if ($http_method === 'POST') {
       curl_setopt($curl, CURLOPT_POST, true);
