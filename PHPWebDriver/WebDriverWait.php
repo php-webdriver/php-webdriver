17b723c7040dfb2d2ec79ed86301c694a651a31d
Adam Goucher <adam@goucher.ca>
2012-02-15 15:07:09 -0500

    adding the license blurb and fixing the polling so it works properly with partial seconds -- which was the default

--- PHPWebDriver/WebDriverWait.php
+++ PHPWebDriver/WebDriverWait.php
@@ -1,4 +1,17 @@
 <?php
+// Copyright 2011-present Element 34
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 include_once('WebDriverExceptions.php');
 
@@ -21,7 +34,7 @@ class PHPWebDriver_WebDriverWait {
             } catch (PHPWebDriver_NoSuchElementWebDriverError $e) {
 
             }
-            sleep($this->poll);
+            usleep(intval($this->poll * 1000000));
         }
         throw new PHPWebDriver_TimeOutWebDriverError(
           sprintf(
