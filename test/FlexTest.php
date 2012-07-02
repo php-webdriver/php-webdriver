f41fa048cc8c00e5f31e2bb04b336b7ac808b009
Adam Goucher <adam@goucher.ca>
2012-07-02 15:37:15 -0400

    port most of the script that comes with the flexstore

--- test/FlexTest.php
+++ test/FlexTest.php
@@ -21,10 +21,7 @@ class FlexTest extends PHPUnit_Framework_TestCase {
     self::$session->close();
   }
   
-  /**
-  * @group adam
-  */
-  public function test_type() {
+  public function test_partially_converted_flexstore_script() {
     $chain = "id:flexstore/name:VBox4/id:storeViews/id:homeView/name:HBox11/name:VBox12/name:Canvas13/name:Canvas14/name:TextInput20/name:UITextField22";
     self::$fp->sendKeys($chain, "nokia");
 
@@ -34,8 +31,35 @@ class FlexTest extends PHPUnit_Framework_TestCase {
     $chain = "name:OK";
     self::$fp->wait_for_object($chain, 5);
     self::$fp->click($chain);
-        
-    // sleep(3);
+   
+    $chain = "id:flexstore/name:VBox*/id:storeViews/id:homeView/name:HBox11/name:VBox12/name:Canvas13/name:Canvas54/name:VBox*/name:HBox*/name:TextInput*/name:UITextField*";
+    self::$fp->click($chain);
+
+    $chain = "id:flexstore/name:VBox*/id:storeViews/id:homeView/name:HBox11/name:VBox12/name:Canvas13/name:Canvas54/name:VBox*/name:HBox*/name:TextInput*/name:UITextField*";
+    self::$fp->sendKeys($chain, "360");
+
+    $chain = "id:flexstore/name:VBox*/id:storeViews/id:homeView/name:HBox*/name:VBox*/name:Canvas*/name:Canvas*/name:VBox*/name:TextInput*/name:UITextField*";
+    self::$fp->sendKeys($chain, "testing");
+
+    $chain = "id:flexstore/name:VBox*/id:storeViews/id:homeView/name:HBox*/name:VBox*/name:Canvas*/name:Canvas*/name:VBox*/name:Button*";
+    self::$fp->click($chain);
+
+    $chain = "name:OK";
+    self::$fp->wait_for_object($chain, 5);
+    self::$fp->click($chain);
+
+    $chain = "id:flexstore/name:VBox4/id:acb/id:_flexstore_ToggleButtonBar1/label:Products";
+    self::$fp->click($chain);
+
+    $chain = "id:flexstore/name:VBox4/id:storeViews/id:pView/name:HBox125/id:filterPanel/name:HBox134/name:TextInput135/name:UITextField137";
+    self::$fp->sendKeys($chain, "20");
+
+    $chain = "id:flexstore/name:VBox4/id:storeViews/id:pView/name:HBox125/id:filterPanel/name:HBox134/name:Button138";
+    self::$fp->click($chain);
+    
+    $chain = "name:OK";
+    self::$fp->wait_for_object($chain, 5);
+    self::$fp->click($chain);
   }
 }
 
