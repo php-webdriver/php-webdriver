3865bf4f626a4f39d9e2b5db436f00cdf224f81c
Adam Goucher <adam@goucher.ca>
2012-08-17 13:29:28 +0200

    adding a test for session/elements

---
+++ test/ElementsTest.php
@@ -0,0 +1,35 @@
+<?php
+require_once(dirname(__FILE__) . '/../PHPWebDriver/WebDriver.php');
+
+class ElementsTest extends PHPUnit_Framework_TestCase {
+  protected static $driver;
+  
+  public function setUp() {
+      self::$driver = new PHPWebDriver_WebDriver();
+  }
+  
+  public function tearDown() {
+      $this->session->close();
+  }
+  
+  /**
+  * @group elements
+  */
+  public function testElementsExist() {
+      $this->session = self::$driver->session(); // firefox
+      $this->session->open("https://github.com/element-34/php-webdriver");
+      $e = $this->session->elements('css selector', '.tabs li');
+      $this->assertEquals(count($e), 7);
+  }
+
+  /**
+  * @group elements
+  */
+  public function testElementsDoesNotExist() {
+      $this->session = self::$driver->session(); // firefox
+      $this->session->open("https://github.com/element-34/php-webdriver");
+      $e = $this->session->elements('css selector', '.flyingmonkey');
+      $this->assertEquals(count($e), 0);
+  }
+}
+?>
