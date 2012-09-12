9d0270cf3d23972ad156e20643c848557e5dece3
Adam Goucher <adam@goucher.ca>
2012-09-11 23:44:36 -0400

    hslToRgba()

--- PHPWebDriver/Support/WebDriverColor.php
+++ PHPWebDriver/Support/WebDriverColor.php
@@ -56,6 +56,44 @@ class PHPWebDriver_Support_Color {
                     $this->red = intval($matches[1] . $matches[1], 16);
                     $this->green = intval($matches[2] . $matches[2], 16);
                     $this->blue = intval($matches[3] . $matches[3], 16);
+                } elseif ($matcher["name"] === "hsl" || $matcher["name"] === "hsla") {
+                    // deal with hsl madness;
+                    $h = $matches[1] / 360;
+                    $s = $matches[2] / 100;
+                    $l = $matches[3] / 100;
+                    if ($s == 0) {
+                        $r = $l;
+                        $g = $r;
+                        $b = $r;
+                    } else {
+                        $hueToRgb = function($luminocity1, $luminocity2, $hue) {
+                            if ($hue < 0.0) {
+                                $hue += 1; 
+                            }
+                            if ($hue > 1.0) {
+                                $hue -= 1;
+                            }
+                            if ($hue < 1.0 / 6.0) {
+                                return ($luminocity1 + ($luminocity2 - $luminocity1) * 6.0 * $hue);
+                            }
+                            if ($hue < 1.0 / 2.0) {
+                                return $luminocity2;
+                            }
+                            if ($hue < 2.0 / 3.0) {
+                                return ($luminocity1 + ($luminocity2 - $luminocity1) * ((2.0 / 3.0) - $hue) * 6.0);
+                            }
+                            return $luminocity1;
+                        };
+                        $luminocity2 = ($l < 0.5) ? $l * (1 + $s) : $l + $s - $l * $s;
+                        $luminocity1 = 2 * $l - $luminocity2;
+                        $r = $hueToRgb($luminocity1, $luminocity2, $h + 1.0 / 3.0);
+                        $g = $hueToRgb($luminocity1, $luminocity2, $h);
+                        $b = $hueToRgb($luminocity1, $luminocity2, $h - 1.0 / 3.0);
+                    }
+                     
+                    $this->red = $r * 256;
+                    $this->green = $g * 256;
+                    $this->blue = $b * 256;
                 } else {
                     // regular things
                     $this->red = $matches[1];
