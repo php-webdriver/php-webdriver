dd2e6e1200f09e19e786186cea52e7abfcd44f53
Adam Goucher <adam@goucher.ca>
2012-09-11 23:01:00 -0400

    getting rid of magic flags

--- PHPWebDriver/Support/WebDriverColor.php
+++ PHPWebDriver/Support/WebDriverColor.php
@@ -17,37 +17,37 @@ class PHPWebDriver_Support_Color {
     public function __construct($color) {
         $matchers = array(
             // rgb %
-            array('/^\s*rgb\(\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*\)\s*$/', true, false, false),
+            array("pattern" => '/^\s*rgb\(\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*\)\s*$/', "name" => "rgb %"),
             // rgb
-            array('/^\s*rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)\s*$/', false, false, false),
+            array("pattern" => '/^\s*rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)\s*$/', "name" => "rgb"),
             // rgba %
-            array('/^\s*rgba\(\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(0|1|0\.\d+)\s*\)\s*$/', true, false, false),
+            array("pattern" => '/^\s*rgba\(\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(\d{1,3}|\d{1,2}\.\d+)%\s*,\s*(0|1|0\.\d+)\s*\)\s*$/', "name" => "rgba %"),
             // rgba
-            array('/^\s*rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0|1|0\.\d+)\s*\)\s*$/', false, false, false),
+            array("pattern" => '/^\s*rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0|1|0\.\d+)\s*\)\s*$/', "name" => "rgba"),
             // hex
-            array('/^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i', false, true, false),
-            // hex3
-            array('/^#([a-f0-9]{1})([a-f0-9]{1})([a-f0-9]{1})$/i', false, true, true),
+            array("pattern" => '/^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i', "name" => "hex"),
+            // hex 3
+            array("pattern" => '/^#([a-f0-9]{1})([a-f0-9]{1})([a-f0-9]{1})$/i', "name" => "hex 3"),
         );
 
         $a = 1;        
         foreach($matchers as $matcher) {
-            preg_match($matcher[0], $color, $matches);
+            preg_match($matcher["pattern"], $color, $matches);
             if (count($matches) != 0) {
                 if (count($matches) == 5) {
                     $a = $matches[4];
                 }
-                if ($matcher[1]) {
+                if ($matcher["name"] === "rgb %" || $matcher["name"] === "rgba %") {
                     // deal with percent values
                     $this->red = floor($matches[1] / 100 * 255);
                     $this->green = floor($matches[2] / 100 * 255);
                     $this->blue = floor($matches[3] / 100 * 255);
-                } elseif ($matcher[2] && !$matcher[3]) {
+                } elseif ($matcher["name"] === "hex") {
                     // deal with hex
                     $this->red = intval($matches[1], 16);
                     $this->green = intval($matches[2], 16);
                     $this->blue = intval($matches[3], 16);
-                } elseif ($matcher[2] && $matcher[3]) {
+                } elseif ($matcher["name"] === "hex 3") {
                     // deal with hex 3
                     $this->red = intval($matches[1] . $matches[1], 16);
                     $this->green = intval($matches[2] . $matches[2], 16);
