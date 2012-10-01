27962f677e857a4d1189682d58eb333cb2c5588e
Lars Golovitcher Petersen <lgp@greenwire.co.uk>
2012-10-01 16:00:31 +0200

    Raise timeout

--- WebDriverBase.php
+++ WebDriverBase.php
@@ -268,7 +268,7 @@ abstract class WebDriverBase {
     return $result;
   }
 
-  function waitForElementPresent($selector, $timeout = 10) {
+  function waitForElementPresent($selector, $timeout = 20) {
     while ($timeout) {
       try {
         $el = $this->find($selector);
