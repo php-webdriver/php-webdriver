cb57c13a5cbe88b5040fd5ad00a06eab87c89219
christian <cbergau86@gmail.com>
2014-01-25 17:53:11 +0100

    First unit test.

---
+++ tests/interactions/internal/WebDriverButtonReleaseActionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../../../lib/__init__.php';
+
+class WebDriverButtonReleaseActionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @type WebDriverButtonReleaseAction
+     */
+    private $webDriverButtonReleaseAction;
+
+    private $webDriverMouse;
+    private $locationProvider;
+
+    public function setUp()
+    {
+        $this->webDriverMouse = $this->getMock('WebDriverMouse');
+        $this->locationProvider = $this->getMock('WebDriverLocatable');
+        $this->webDriverButtonReleaseAction = new WebDriverButtonReleaseAction(
+            $this->webDriverMouse,
+            $this->locationProvider
+        );
+    }
+
+    public function testPerformSendsMouseUpCommand()
+    {
+        $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
+        $this->webDriverMouse->expects($this->once())->method('mouseUp');
+        $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
+        $this->webDriverButtonReleaseAction->perform();
+    }
+}
