bd65ff6666c229bea930b3cde3019352f948339d
christian <cbergau86@gmail.com>
2014-01-25 19:37:36 +0100

    Add unit test for WebDriverContextClickAction

---
+++ tests/interactions/internal/WebDriverContextClickActionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class WebDriverContextClickActionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @type WebDriverContextClickAction
+     */
+    private $webDriverContextClickAction;
+
+    private $webDriverMouse;
+    private $locationProvider;
+
+    public function setUp()
+    {
+        $this->webDriverMouse = $this->getMock('WebDriverMouse');
+        $this->locationProvider = $this->getMock('WebDriverLocatable');
+        $this->webDriverContextClickAction = new WebDriverContextClickAction(
+            $this->webDriverMouse,
+            $this->locationProvider
+        );
+    }
+
+    public function testPerformSendsMouseDownCommand()
+    {
+        $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
+        $this->webDriverMouse->expects($this->once())->method('contextClick');
+        $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
+        $this->webDriverContextClickAction->perform();
+    }
+}
