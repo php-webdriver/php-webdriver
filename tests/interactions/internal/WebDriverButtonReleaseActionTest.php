abc1bbae4ca9ce94ef0be7cc4b645c9b8294c074
christian <cbergau86@gmail.com>
2014-01-28 17:41:06 +0100

    Fixed code style

--- tests/interactions/internal/WebDriverButtonReleaseActionTest.php
+++ tests/interactions/internal/WebDriverButtonReleaseActionTest.php
@@ -2,29 +2,27 @@
 
 class WebDriverButtonReleaseActionTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @type WebDriverButtonReleaseAction
-     */
-    private $webDriverButtonReleaseAction;
+  /**
+   * @type WebDriverButtonReleaseAction
+   */
+  private $webDriverButtonReleaseAction;
 
-    private $webDriverMouse;
-    private $locationProvider;
+  private $webDriverMouse;
+  private $locationProvider;
 
-    public function setUp()
-    {
-        $this->webDriverMouse = $this->getMock('WebDriverMouse');
-        $this->locationProvider = $this->getMock('WebDriverLocatable');
-        $this->webDriverButtonReleaseAction = new WebDriverButtonReleaseAction(
-            $this->webDriverMouse,
-            $this->locationProvider
-        );
-    }
+  public function setUp() {
+    $this->webDriverMouse = $this->getMock('WebDriverMouse');
+    $this->locationProvider = $this->getMock('WebDriverLocatable');
+    $this->webDriverButtonReleaseAction = new WebDriverButtonReleaseAction(
+      $this->webDriverMouse,
+      $this->locationProvider
+    );
+  }
 
-    public function testPerformSendsMouseUpCommand()
-    {
-        $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
-        $this->webDriverMouse->expects($this->once())->method('mouseUp');
-        $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
-        $this->webDriverButtonReleaseAction->perform();
-    }
+  public function testPerformSendsMouseUpCommand() {
+    $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
+    $this->webDriverMouse->expects($this->once())->method('mouseUp');
+    $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
+    $this->webDriverButtonReleaseAction->perform();
+  }
 }
--- tests/interactions/internal/WebDriverClickActionTest.php
+++ tests/interactions/internal/WebDriverClickActionTest.php
@@ -2,29 +2,27 @@
 
 class WebDriverClickActionTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @type WebDriverButtonReleaseAction
-     */
-    private $webDriverClickAction;
+  /**
+   * @type WebDriverButtonReleaseAction
+   */
+  private $webDriverClickAction;
 
-    private $webDriverMouse;
-    private $locationProvider;
+  private $webDriverMouse;
+  private $locationProvider;
 
-    public function setUp()
-    {
-        $this->webDriverMouse = $this->getMock('WebDriverMouse');
-        $this->locationProvider = $this->getMock('WebDriverLocatable');
-        $this->webDriverClickAction = new WebDriverClickAction(
-            $this->webDriverMouse,
-            $this->locationProvider
-        );
-    }
+  public function setUp() {
+    $this->webDriverMouse = $this->getMock('WebDriverMouse');
+    $this->locationProvider = $this->getMock('WebDriverLocatable');
+    $this->webDriverClickAction = new WebDriverClickAction(
+      $this->webDriverMouse,
+      $this->locationProvider
+    );
+  }
 
-    public function testPerformSendsClickCommand()
-    {
-        $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
-        $this->webDriverMouse->expects($this->once())->method('click');
-        $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
-        $this->webDriverClickAction->perform();
-    }
+  public function testPerformSendsClickCommand() {
+    $coords = $this->getMockBuilder('WebDriverCoordinates')->disableOriginalConstructor()->getMock();
+    $this->webDriverMouse->expects($this->once())->method('click');
+    $this->locationProvider->expects($this->once())->method('getCoordinates')->will($this->returnValue($coords));
+    $this->webDriverClickAction->perform();
+  }
 }
