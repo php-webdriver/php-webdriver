c90b3a9f6bf833526c2069bf6a74c332f670a324
EvanCoh <cohylakis@gmail.com>
2015-08-03 23:06:54 +0300

    unit tests wrong namespace deletion

--- tests/unit/WebDriverDoubleClickActionTest.php
+++ tests/unit/WebDriverDoubleClickActionTest.php
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Facebook\WebDriver;
-
 use Facebook\WebDriver\Interactions\Internal\WebDriverDoubleClickAction;
 
 class WebDriverDoubleClickActionTest extends \PHPUnit_Framework_TestCase {
