3596ddca6cac42bca6a8369baa2cc94d382caaae
Punk <jump.e.r@yandex.ru>
2015-12-21 17:39:05 +0300

    Timeout 120000

--- lib/Remote/HttpCommandExecutor.php
+++ lib/Remote/HttpCommandExecutor.php
@@ -150,8 +150,8 @@ class HttpCommandExecutor implements WebDriverCommandExecutor {
         'Accept: application/json',
       )
     );
-    $this->setRequestTimeout(30000);
-    $this->setConnectionTimeout(30000);
+    $this->setRequestTimeout(120000);
+    $this->setConnectionTimeout(120000);
   }
 
   /**
