865e9b2c44eb8b1d7f21b4149f319875e5681d1e
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-01-27 20:34:00 +0100

    Add unit test to cookie setting and retrieval in WebDriverOptions class

---
+++ tests/unit/WebDriverOptionsTest.php
@@ -0,0 +1,152 @@
+<?php
+// Copyright 2004-present Facebook. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Facebook\WebDriver;
+
+use Facebook\WebDriver\Remote\DriverCommand;
+use Facebook\WebDriver\Remote\ExecuteMethod;
+
+/**
+ * @covers Facebook\WebDriver\WebDriverOptions
+ */
+class WebDriverOptionsTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ExecuteMethod|\PHPUnit_Framework_MockObject_MockObject */
+    private $executor;
+
+    public function setUp()
+    {
+        $this->executor = $this->getMockBuilder(ExecuteMethod::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testShouldAddCookieFromArray()
+    {
+        $cookieInArray = [
+            'name' => 'cookieName',
+            'value' => 'someValue',
+            'path' => '/bar',
+            'domain' => 'foo',
+            'expiry' => 1485388333,
+            'secure' => false,
+            'httpOnly' => false,
+        ];
+
+        $this->executor->expects($this->once())
+            ->method('execute')
+            ->with(DriverCommand::ADD_COOKIE, ['cookie' => $cookieInArray]);
+
+        $options = new WebDriverOptions($this->executor);
+
+        $options->addCookie($cookieInArray);
+    }
+
+    public function testShouldGetAllCookies()
+    {
+        $this->executor->expects($this->once())
+            ->method('execute')
+            ->with(DriverCommand::GET_ALL_COOKIES)
+            ->willReturn(
+                [
+                    [
+                        'path' => '/',
+                        'domain' => '*.seleniumhq.org',
+                        'name' => 'firstCookie',
+                        'httpOnly' => false,
+                        'secure' => true,
+                        'value' => 'value',
+                    ],
+                    [
+                        'path' => '/',
+                        'domain' => 'docs.seleniumhq.org',
+                        'name' => 'secondCookie',
+                        'httpOnly' => false,
+                        'secure' => false,
+                        'value' => 'foo',
+                    ],
+                ]
+            );
+
+        $options = new WebDriverOptions($this->executor);
+
+        $cookies = $options->getCookies();
+
+        $this->assertCount(2, $cookies);
+        $this->assertSame('firstCookie', $cookies[0]['name']);
+        $this->assertSame('secondCookie', $cookies[1]['name']);
+    }
+
+    public function testShouldGetCookieByName()
+    {
+        $this->executor->expects($this->once())
+            ->method('execute')
+            ->with(DriverCommand::GET_ALL_COOKIES)
+            ->willReturn(
+                [
+                    [
+                        'path' => '/',
+                        'domain' => '*.seleniumhq.org',
+                        'name' => 'cookieToFind',
+                        'httpOnly' => false,
+                        'secure' => true,
+                        'value' => 'value',
+                    ],
+                    [
+                        'path' => '/',
+                        'domain' => 'docs.seleniumhq.org',
+                        'name' => 'otherCookie',
+                        'httpOnly' => false,
+                        'secure' => false,
+                        'value' => 'foo',
+                    ],
+                ]
+            );
+
+        $options = new WebDriverOptions($this->executor);
+
+        $cookie = $options->getCookieNamed('cookieToFind');
+
+        $this->assertSame('cookieToFind', $cookie['name']);
+        $this->assertSame('value', $cookie['value']);
+        $this->assertSame('/', $cookie['path']);
+        $this->assertSame('*.seleniumhq.org', $cookie['domain']);
+        $this->assertFalse($cookie['httpOnly']);
+        $this->assertTrue($cookie['secure']);
+    }
+
+    public function testShouldReturnNullIfCookieWithNameNotFound()
+    {
+        $this->executor->expects($this->once())
+            ->method('execute')
+            ->with(DriverCommand::GET_ALL_COOKIES)
+            ->willReturn(
+                [
+                    [
+                        'path' => '/',
+                        'domain' => '*.seleniumhq.org',
+                        'name' => 'cookieToNotFind',
+                        'httpOnly' => false,
+                        'secure' => true,
+                        'value' => 'value',
+                    ],
+                ]
+            );
+
+        $options = new WebDriverOptions($this->executor);
+
+        $this->assertNull($options->getCookieNamed('notExistingCookie'));
+    }
+}
