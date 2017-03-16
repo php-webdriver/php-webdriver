89a6ce780387202ccbdb511eab613a5691613034
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-03-16 13:02:52 +0000

    Workaround in example script for Selenium bug (fixes #402)

--- example.php
+++ example.php
@@ -13,8 +13,8 @@ $host = 'http://localhost:4444/wd/hub'; // this is the default
 $capabilities = DesiredCapabilities::firefox();
 $driver = RemoteWebDriver::create($host, $capabilities, 5000);
 
-// navigate to 'http://docs.seleniumhq.org/'
-$driver->get('http://docs.seleniumhq.org/');
+// navigate to 'http://www.seleniumhq.org/'
+$driver->get('http://www.seleniumhq.org/');
 
 // adding cookie
 $driver->manage()->deleteAllCookies();
@@ -31,17 +31,24 @@ $link = $driver->findElement(
 );
 $link->click();
 
+// wait until the page is loaded
+$driver->wait()->until(
+    WebDriverExpectedCondition::titleContains('About')
+);
+
 // print the title of the current page
 echo "The title is '" . $driver->getTitle() . "'\n";
 
 // print the URI of the current page
 echo "The current URI is '" . $driver->getCurrentURL() . "'\n";
 
-// Search 'php' in the search box
-$input = $driver->findElement(
-    WebDriverBy::id('q')
-);
-$input->sendKeys('php')->submit();
+// write 'php' in the search box
+$driver->findElement(WebDriverBy::id('q'))
+    ->sendKeys('php');
+
+// submit the form
+$driver->findElement(WebDriverBy::id('submit'))
+    ->click(); // submit() does not work in Selenium 3 because of bug https://github.com/SeleniumHQ/selenium/issues/3398
 
 // wait at most 10 seconds until at least one result is shown
 $driver->wait(10)->until(
