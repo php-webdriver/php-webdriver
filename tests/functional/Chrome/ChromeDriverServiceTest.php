cae27962e3b1e4fae70c951769c00204bd927eca
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-10-25 23:05:52 +0200

    Add tests for ChromeDriverService and ChromeDriver

---
+++ tests/functional/Chrome/ChromeDriverServiceTest.php
@@ -0,0 +1,82 @@
+<?php
+// Copyright 2004-present Facebook. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Facebook\WebDriver\Chrome;
+
+/**
+ * @group exclude-saucelabs
+ * @covers Facebook\WebDriver\Chrome\ChromeDriverService
+ * @covers Facebook\WebDriver\Remote\Service\DriverService
+ */
+class ChromeDriverServiceTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        if (!getenv('BROWSER_NAME') === 'chrome' || getenv('SAUCELABS') || !getenv('CHROMEDRIVER_PATH')) {
+            $this->markTestSkipped('ChromeDriverServiceTest is run only when running against local chrome');
+        }
+    }
+
+    public function testShouldStartAndStopServiceCreatedUsingShortcutConstructor()
+    {
+        // The createDefaultService() method expect path to the executable to be present in the environment variable
+        putenv(ChromeDriverService::CHROME_DRIVER_EXE_PROPERTY . '=' . getenv('CHROMEDRIVER_PATH'));
+
+        $driverService = ChromeDriverService::createDefaultService();
+
+        $this->assertSame('http://localhost:9515', $driverService->getURL());
+
+        $this->assertInstanceOf(ChromeDriverService::class, $driverService->start());
+        $this->assertTrue($driverService->isRunning());
+
+        $this->assertInstanceOf(ChromeDriverService::class, $driverService->start());
+
+        $this->assertInstanceOf(ChromeDriverService::class, $driverService->stop());
+        $this->assertFalse($driverService->isRunning());
+
+        $this->assertInstanceOf(ChromeDriverService::class, $driverService->stop());
+    }
+
+    public function testShouldStartAndStopServiceCreatedUsingDefaultConstructor()
+    {
+        $driverService = new ChromeDriverService(getenv('CHROMEDRIVER_PATH'), 9515, ['--port=9515']);
+
+        $this->assertSame('http://localhost:9515', $driverService->getURL());
+
+        $driverService->start();
+        $this->assertTrue($driverService->isRunning());
+
+        $driverService->stop();
+        $this->assertFalse($driverService->isRunning());
+    }
+
+    public function testShouldThrowExceptionIfExecutableCannotBeFound()
+    {
+        putenv(ChromeDriverService::CHROME_DRIVER_EXE_PROPERTY . '=/not/existing');
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('\'/not/existing\' is not a file.');
+        ChromeDriverService::createDefaultService();
+    }
+
+    public function testShouldThrowExceptionIfExecutableIsNotExecutable()
+    {
+        putenv(ChromeDriverService::CHROME_DRIVER_EXE_PROPERTY . '=' . __FILE__);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('is not executable');
+        ChromeDriverService::createDefaultService();
+    }
+}
---
+++ tests/functional/Chrome/ChromeDriverTest.php
@@ -0,0 +1,60 @@
+<?php
+// Copyright 2004-present Facebook. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Facebook\WebDriver\Chrome;
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\Service\DriverCommandExecutor;
+
+/**
+ * @group exclude-saucelabs
+ * @covers Facebook\WebDriver\Chrome\ChromeDriver
+ */
+class ChromeDriverTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ChromeDriver */
+    protected $driver;
+
+    protected function setUp()
+    {
+        if (!getenv('BROWSER_NAME') === 'chrome' || getenv('SAUCELABS') || !getenv('CHROMEDRIVER_PATH')) {
+            $this->markTestSkipped('ChromeDriverServiceTest is run only when running against local chrome');
+        }
+    }
+
+    protected function tearDown()
+    {
+        if ($this->driver instanceof RemoteWebDriver && $this->driver->getCommandExecutor() !== null) {
+            $this->driver->quit();
+        }
+    }
+
+    public function testShouldStartChromeDriver()
+    {
+        // The createDefaultService() method expect path to the executable to be present in the environment variable
+        putenv(ChromeDriverService::CHROME_DRIVER_EXE_PROPERTY . '=' . getenv('CHROMEDRIVER_PATH'));
+
+        $this->driver = ChromeDriver::start();
+
+        $this->assertInstanceOf(ChromeDriver::class, $this->driver);
+        $this->assertInstanceOf(DriverCommandExecutor::class, $this->driver->getCommandExecutor());
+
+        $this->driver->get('http://localhost:8000/');
+
+        $this->assertSame('http://localhost:8000/', $this->driver->getCurrentURL());
+
+        $this->driver->quit();
+    }
+}
