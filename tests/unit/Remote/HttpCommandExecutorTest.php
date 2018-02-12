372fcba2b549a2f7a982e5eacb45614961dcddd5
Antonin Rykalsky <antonin.rykalsky@dixonscarphone.com>
2018-02-12 12:58:01 +0100

    Disable sending Expect header in POST requests

--- tests/unit/Remote/HttpCommandExecutorTest.php
+++ tests/unit/Remote/HttpCommandExecutorTest.php
@@ -34,19 +34,32 @@ class HttpCommandExecutorTest extends TestCase
      * @dataProvider commandProvider
      * @param int $command
      * @param array $params
+     * @param bool $shouldResetExpectHeader
      * @param string $expectedUrl
      * @param string $expectedPostData
      */
-    public function testShouldSendRequestToAssembledUrl($command, array $params, $expectedUrl, $expectedPostData)
-    {
+    public function testShouldSendRequestToAssembledUrl(
+        $command,
+        array $params,
+        $shouldResetExpectHeader,
+        $expectedUrl,
+        $expectedPostData
+    ) {
         $command = new WebDriverCommand('foo-123', $command, $params);
 
         $curlSetoptMock = $this->getFunctionMock(__NAMESPACE__, 'curl_setopt');
         $curlSetoptMock->expects($this->at(0))
             ->with($this->anything(), CURLOPT_URL, $expectedUrl);
 
-        $curlSetoptMock->expects($this->at(2))
-            ->with($this->anything(), CURLOPT_POSTFIELDS, $expectedPostData);
+        if ($shouldResetExpectHeader) {
+            $curlSetoptMock->expects($this->at(2))
+                ->with($this->anything(), CURLOPT_HTTPHEADER, ['Expect:']);
+            $curlSetoptMock->expects($this->at(3))
+                ->with($this->anything(), CURLOPT_POSTFIELDS, $expectedPostData);
+        } else {
+            $curlSetoptMock->expects($this->at(2))
+                ->with($this->anything(), CURLOPT_POSTFIELDS, $expectedPostData);
+        }
 
         $curlExecMock = $this->getFunctionMock(__NAMESPACE__, 'curl_exec');
         $curlExecMock->expects($this->once())
@@ -64,30 +77,35 @@ class HttpCommandExecutorTest extends TestCase
             'POST command having :id placeholder in url' => [
                 DriverCommand::SEND_KEYS_TO_ELEMENT,
                 ['value' => 'submitted-value', ':id' => '1337'],
+                true,
                 'http://localhost:4444/session/foo-123/element/1337/value',
                 '{"value":"submitted-value"}',
             ],
             'POST command without :id placeholder in url' => [
                 DriverCommand::TOUCH_UP,
                 ['x' => 3, 'y' => 6],
+                true,
                 'http://localhost:4444/session/foo-123/touch/up',
                 '{"x":3,"y":6}',
             ],
             'Extra useless placeholder parameter should be removed' => [
                 DriverCommand::TOUCH_UP,
                 ['x' => 3, 'y' => 6, ':useless' => 'foo'],
+                true,
                 'http://localhost:4444/session/foo-123/touch/up',
                 '{"x":3,"y":6}',
             ],
             'DELETE command' => [
                 DriverCommand::DELETE_COOKIE,
                 [':name' => 'cookie-name'],
+                false,
                 'http://localhost:4444/session/foo-123/cookie/cookie-name',
                 null,
             ],
             'GET command without session in URL' => [
                 DriverCommand::GET_ALL_SESSIONS,
                 [],
+                false,
                 'http://localhost:4444/sessions',
                 null,
             ],
