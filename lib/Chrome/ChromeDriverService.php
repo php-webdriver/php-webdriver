4fb525ba17a8ee247e15fb9be320817eb3bd7630
Rob <rob.rimmer@mclaren.com>
2018-03-05 10:02:54 +0000

    Change variable name defining path to ChromeDriver executable
    1. Why is this change necessary?
    Current environment variable in ChromeDriver.php uses '.', whilst valid according to the specification, this will not work using a bash shell (probably the most commonly used)
    
    2. How does it address the issue?
    '.' replaced with '_'.  This is usable in bash (including cygwin)
    Original code still there, only attempts new variable if original does not exist
    
    3. What are the side effects?
    None, previous name will still work, new name only used if original name does not exist

--- lib/Chrome/ChromeDriverService.php
+++ lib/Chrome/ChromeDriverService.php
@@ -19,19 +19,23 @@ use Facebook\WebDriver\Remote\Service\DriverService;
 
 class ChromeDriverService extends DriverService
 {
-    // The environment variable storing the path to the chrome driver executable.
+    /**
+     * The environment variable storing the path to the chrome driver executable.
+     * @deprecated Use ChromeDriverService::CHROME_DRIVER_EXECUTABLE
+     */
     const CHROME_DRIVER_EXE_PROPERTY = 'webdriver.chrome.driver';
+    // The environment variable storing the path to the chrome driver executable
+    const CHROME_DRIVER_EXECUTABLE = 'WEBDRIVER_CHROME_DRIVER';
 
     /**
      * @return static
      */
     public static function createDefaultService()
     {
-        $exe = getenv(self::CHROME_DRIVER_EXE_PROPERTY);
+        $exe = getenv(self::CHROME_DRIVER_EXECUTABLE) ?: getenv(self::CHROME_DRIVER_EXE_PROPERTY);
         $port = 9515; // TODO: Get another port if the default port is used.
-        $args = ["--port=$port"];
-        $service = new static($exe, $port, $args);
+        $args = ['--port=' . $port];
 
-        return $service;
+        return new static($exe, $port, $args);
     }
 }
