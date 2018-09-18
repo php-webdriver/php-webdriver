a5ef18fe452ae69d16c5032fd36c6f6ab0322626
Michal Pierzchalski <mpierzchalski@getresponse.com>
2018-09-18 15:38:26 +0200

    update travis and licence

--- lib/Remote/Translator/W3CProtocolTranslator.php
+++ lib/Remote/Translator/W3CProtocolTranslator.php
@@ -1,4 +1,17 @@
 <?php
+// Copyright 2004-present Facebook. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Facebook\WebDriver\Remote\Translator;
 
@@ -11,6 +24,8 @@ use Facebook\WebDriver\Support\W3CKeysEncoder;
 class W3CProtocolTranslator implements WebDriverProtocolTranslator
 {
     const ELEMENT_FILED = 'element-6066-11e4-a52e-4f735466cecf';
+
+    const RESOURCES_DIR = __DIR__ . '/../../../resources';
 
     /**
      * @see https://w3c.github.io/webdriver/#elements
@@ -271,7 +286,7 @@ class W3CProtocolTranslator implements WebDriverProtocolTranslator
             [
                 'script' => sprintf(
                     'return (%s).apply(null, arguments);',
-                    file_get_contents(__DIR__ . '/resources/getAttribute.js')
+                    file_get_contents(self::RESOURCES_DIR . '/getAttribute.js')
                 ),
                 'args' => [
                     [self::ELEMENT_FILED => $command->getParameters()[':id']],
@@ -309,7 +324,7 @@ class W3CProtocolTranslator implements WebDriverProtocolTranslator
             [
                 'script' => sprintf(
                     'return (%s).apply(null, arguments);',
-                    file_get_contents(__DIR__ . '/resources/isDisplayed.js')
+                    file_get_contents(self::RESOURCES_DIR . '/isDisplayed.js')
                 ),
                 'args' => [
                     [self::ELEMENT_FILED => $command->getParameters()[':id']],
