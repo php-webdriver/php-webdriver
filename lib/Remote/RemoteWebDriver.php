f650ee73645a3e95fb0567b25d4f33c13664cbde
Ondřej Machulda <ondrej.machulda@gmail.com>
2019-06-12 11:32:08 +0200

    Do not send w3c capability as a workaround for browsestack schema validation (fixes #644)

--- lib/Remote/RemoteWebDriver.php
+++ lib/Remote/RemoteWebDriver.php
@@ -105,7 +105,9 @@ class RemoteWebDriver implements WebDriver, JavaScriptExecutor, WebDriverHasInpu
 
         // Hotfix: W3C WebDriver protocol is not yet supported by php-webdriver, so we must force Chromedriver to
         // not use the W3C protocol by default (which is what Chromedriver does starting with version 75).
-        if ($desired_capabilities->getBrowserName() === WebDriverBrowserType::CHROME) {
+        if ($desired_capabilities->getBrowserName() === WebDriverBrowserType::CHROME
+            && mb_strpos($selenium_server_url, 'browserstack') === false // see https://github.com/facebook/php-webdriver/issues/644
+        ) {
             $currentChromeOptions = $desired_capabilities->getCapability(ChromeOptions::CAPABILITY);
             $chromeOptions = !empty($currentChromeOptions) ? $currentChromeOptions : new ChromeOptions();
 
