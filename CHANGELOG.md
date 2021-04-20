2ea92878bc1367226003f4799d831dfa2ae43ad9
Ondřej Machulda <ondrej.machulda@gmail.com>
2021-04-20 15:55:07 +0200

    Fix browser capabilities to be set to driver instance when using ChromeDriver::create

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,17 @@
 This project versioning adheres to [Semantic Versioning](http://semver.org/).
 
 ## Unreleased
-- Add `FirefoxOptions` class to simplify passing Firefox capabilities. Usage is covered [in our wiki](https://github.com/php-webdriver/php-webdriver/wiki/Firefox#firefoxoptions).`
-- Add `FirefoxDriver` to easy local start of Firefox instance without a need to start the `geckodriver` process manually.
+
+### Added
+- `FirefoxOptions` class to simplify passing Firefox capabilities. Usage is covered [in our wiki](https://github.com/php-webdriver/php-webdriver/wiki/Firefox#firefoxoptions).`
+- `FirefoxDriver` to easy local start of Firefox instance without a need to start the `geckodriver` process manually.
+- Method `ChromeDriver::startUsingDriverService()` to be used for creating ChromeDriver instance with custom service.
+
+### Fixed
+- Driver capabilities received from the browser when creating now session were not set to the instance of ChromeDriver (when ChromeDriver::start() was used).
+
+### Changed
+- Deprecate `ChromeDriver::startSession`. However the method was supposed to be used only internally.
 
 ## 1.10.0 - 2021-02-25
 ### Added
--- tests/functional/Chrome/ChromeDriverTest.php
+++ tests/functional/Chrome/ChromeDriverTest.php
@@ -33,9 +33,13 @@ class ChromeDriverTest extends TestCase
         }
     }
 
-    public function testShouldStartChromeDriver()
+    /**
+     * @dataProvider provideDialect
+     * @param bool $isW3cDialect
+     */
+    public function testShouldStartChromeDriver($isW3cDialect)
     {
-        $this->startChromeDriver();
+        $this->startChromeDriver($isW3cDialect);
         $this->assertInstanceOf(ChromeDriver::class, $this->driver);
         $this->assertInstanceOf(DriverCommandExecutor::class, $this->driver->getCommandExecutor());
 
@@ -46,6 +50,17 @@ class ChromeDriverTest extends TestCase
         $this->driver->get('http://localhost:8000/');
 
         $this->assertSame('http://localhost:8000/', $this->driver->getCurrentURL());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function provideDialect()
+    {
+        return [
+            'w3c' => [true],
+            'oss' => [false],
+        ];
     }
 
     public function testShouldInstantiateDevTools()
@@ -66,7 +81,7 @@ class ChromeDriverTest extends TestCase
         $this->assertSame(['result' => ['type' => 'string', 'value' => 'http://localhost:8000/']], $cdpResult);
     }
 
-    private function startChromeDriver()
+    private function startChromeDriver($w3cDialect = true)
     {
         // The createDefaultService() method expect path to the executable to be present in the environment variable
         putenv(ChromeDriverService::CHROME_DRIVER_EXECUTABLE . '=' . getenv('CHROMEDRIVER_PATH'));
@@ -74,6 +89,7 @@ class ChromeDriverTest extends TestCase
         // Add --no-sandbox as a workaround for Chrome crashing: https://github.com/SeleniumHQ/selenium/issues/4961
         $chromeOptions = new ChromeOptions();
         $chromeOptions->addArguments(['--no-sandbox', '--headless']);
+        $chromeOptions->setExperimentalOption('w3c', $w3cDialect);
         $desiredCapabilities = DesiredCapabilities::chrome();
         $desiredCapabilities->setCapability(ChromeOptions::CAPABILITY, $chromeOptions);
 
