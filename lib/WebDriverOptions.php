cccee01cbac9bbf86fde21c31b688bf149b26554
Volk <118300374+volk-r@users.noreply.github.com>
2023-01-17 10:18:38 +0500

    code style

--- lib/WebDriverOptions.php
+++ lib/WebDriverOptions.php
@@ -125,13 +125,13 @@ class WebDriverOptions
 
         $cookies = [];
         foreach ($cookieArrays as $cookieArray) {
-            // InvalidArgumentException: Cookie name should be non-empty trace
+            // InvalidArgumentException: Cookie name should be non-empty trace:
             if (is_object($cookieArray)) {
                 /** @var \Facebook\WebDriver\Cookie $cookieArray */
                 $cookieObj = $cookieArray;
                 $cookieArray = $cookieObj->toArray();
             }
-            
+
             $cookies[] = Cookie::createFromArray($cookieArray);
         }
 
