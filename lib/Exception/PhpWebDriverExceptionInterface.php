dadaa12f76a322dae98b794b67507913616e7e5e
Andrew Alyamovsky <andrew.alyamovsky@gmail.com>
2025-02-13 16:52:32 +0100

    Add Throwable extension to PhpWebDriverExceptionInterface

--- lib/Exception/PhpWebDriverExceptionInterface.php
+++ lib/Exception/PhpWebDriverExceptionInterface.php
@@ -5,6 +5,6 @@ namespace Facebook\WebDriver\Exception;
 /**
  * Common interface to identify all exceptions thrown in php-webdriver (both those of WebDriver protocol and internal).
  */
-interface PhpWebDriverExceptionInterface
+interface PhpWebDriverExceptionInterface extends \Throwable
 {
 }
